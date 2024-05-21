11173b9b89ec232d06849fc15d09d28a9313b622
Abdul Haseeb A <abdul44haseeb@gmail.com>
2024-05-21 21:29:08 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Abdul Haseeb',
+    author_email= 'abdul44haseeb@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
